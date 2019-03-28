faccc768274116833e2a176af2ad7aef6cad17b0
minimacro <joachimpol@yahoo.de>
2019-03-28 22:44:47 +0100

    Created basic skeleton of project

---
+++ src/main/java/at/fhj/iit/CalculatorImpl.java
@@ -0,0 +1,20 @@
+package at.fhj.iit;
+import java.util.ArrayList;
+import java.util.List;
+public class CalculatorImpl implements Calculator {
+
+
+
+    public int getMaximum() {
+        return 0;
+    }
+
+    public int getMinimum() {
+        return 0;
+    }
+
+    public void addValue(int value) {
+
+    }
+}
+
---
+++ src/main/java/at/fhj/iit/Main.java
@@ -0,0 +1,23 @@
+package at.fhj.iit;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculator calc = new CalculatorImpl();
+
+        calc.addValue(20);	// currently highest number
+        calc.addValue(4);	// currently lowester number
+        calc.addValue(16);
+
+        // add other integers
+
+        // output lowest number
+
+        // output highest number
+
+        // output sum of all numbers
+
+    }
+
+}
